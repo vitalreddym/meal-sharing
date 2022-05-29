59a3b08c9548473b8d1c2aeb225d40f60cdb7e7e
vitalreddymynampati <vitalreddym@gmail.com>
2022-05-29 23:13:54 +0200

    updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -43,7 +43,7 @@ module.exports = {
     port: parseInt(process.env.CLIENT_PORT, 10),
     open: process.env.OPEN_BROWSER === 'true' ? true : false,
     proxy: {
-      '/api': `http://localhost:${process.env.API_URL}`,
+      '/api': `http://localhost:${process.env.API_PORT}`,
     },
   },
   node: {
