d384b63ed00694340ff0c60abcfa37059060f8f3
vitalreddymynampati <vitalreddym@gmail.com>
2022-06-01 20:01:41 +0200

    upd

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,10 +1,9 @@
-/*const app = require("./app");
+const app = require("./app");
 
 const port = parseInt(process.env.PORT, 10) || process.env.API_PORT;
 
 app.listen(port, () => {
-  //console.log(`Backend api available at ${process.env.API_PATH}`);
+  console.log(`Backend api available at ${process.env.API_PATH}`);
 });
- */
+ 
 
-console.log("Hello World");
