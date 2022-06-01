70b8fcddca7935b3eee6c072ffc8d650f6287ea4
vitalreddymynampati <vitalreddym@gmail.com>
2022-06-01 22:00:01 +0200

    updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const path = require('path');
-//const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
+const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const outputDirectory = 'dist';
@@ -63,7 +63,7 @@ module.exports = {
       favicon: './public/favicon.ico',
     }),
 
-   // new CaseSensitivePathsPlugin(),
+    new CaseSensitivePathsPlugin(),
     new Dotenv({
       safe: false,
     }),
